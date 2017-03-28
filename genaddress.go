f4e7ade0aedb76de626a7dec15bed658bacf4ee5
Adrià Massanet <amb@mac15.local>
2017-03-28 18:42:10 +0200

    format code

--- genaddress.go
+++ genaddress.go
@@ -1,75 +1,71 @@
 package main
 
 import (
-
-    "os"
-    "strings"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"encoding/hex"
 	"log"
-    "strconv"
-    "math"
-    "crypto/ecdsa"
-    "crypto/rand"
-    "encoding/hex"
-    "sync/atomic"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"sync/atomic"
 
-    "github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/ethereum/go-ethereum/crypto"
-
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
 func main() {
 
-    if len(os.Args) != 3 {
-        log.Fatal("Usage genaddress <concurrency> <addressprefix>")
-    }
+	if len(os.Args) != 3 {
+		log.Fatal("Usage genaddress <concurrency> <addressprefix>")
+	}
 
-    concurrency,err := strconv.Atoi(os.Args[1])
-    if err != nil {
-        log.Fatal("Bad concurrency parameter")
-    }
-    prefix := os.Args[2]
+	concurrency, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal("Bad concurrency parameter")
+	}
+	prefix := os.Args[2]
 
-    var combinations = math.Pow(16,float64(len(prefix)))
+	var combinations = math.Pow(16, float64(len(prefix)))
 
-    log.Println("Generating key with address starting with",prefix,"using",concurrency,"threads")
-    log.Println("Total combinations are",uint64(combinations))
+	log.Println("Generating key with address starting with", prefix, "using", concurrency, "threads")
+	log.Println("Total combinations are", uint64(combinations))
 
-    var counter uint64 = 0
-    var finished = make (chan bool)
+	var counter uint64 = 0
+	var finished = make(chan bool)
 
-    for i := 0 ; i < concurrency ; i++ {
+	for i := 0; i < concurrency; i++ {
 
-        go func() {
+		go func() {
 
-            for {
+			for {
 
-                atomic.AddUint64(&counter, 1)
-                if counter % 25000 == 0 {
-                    current := (float64(counter) * 100) / combinations
-                    log.Printf("%v (%.3f %%)",counter,current)
-                }
+				atomic.AddUint64(&counter, 1)
+				if counter%25000 == 0 {
+					current := (float64(counter) * 100) / combinations
+					log.Printf("%v (%.3f %%)", counter, current)
+				}
 
-                k, err := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
-                if err != nil {
-                    log.Fatal(err);
-                }
+				k, err := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
+				if err != nil {
+					log.Fatal(err)
+				}
 
-                address := crypto.PubkeyToAddress(k.PublicKey)
-                addressStr := hex.EncodeToString(address[:])
+				address := crypto.PubkeyToAddress(k.PublicKey)
+				addressStr := hex.EncodeToString(address[:])
 
-                if strings.HasPrefix(addressStr, os.Args[2]) {
+				if strings.HasPrefix(addressStr, os.Args[2]) {
 
-                    pvkStr := hex.EncodeToString(crypto.FromECDSA(k))
-                    log.Println("\n\nFOUND address=[",addressStr, "] pvk=[",pvkStr,"]")
+					pvkStr := hex.EncodeToString(crypto.FromECDSA(k))
+					log.Println("\n\nFOUND address=[", addressStr, "] pvk=[", pvkStr, "]")
 
-                    finished <- true
+					finished <- true
 
-                }
-            }
-        }()
-    }
+				}
+			}
+		}()
+	}
 
-    <- finished
+	<-finished
 }
-
-
